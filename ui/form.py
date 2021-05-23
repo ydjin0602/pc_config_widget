2ebe2e33f3d8b88a91f38d56359db748e19e0b3b
ydjin <e.mishchenko@chatme.ai>
2021-05-23 17:11:38 +0700

    changed multiprocession on threading, all cases of receiving message boxes have been worked out

--- ui/form.py
+++ ui/form.py
@@ -1,4 +1,4 @@
-import multiprocessing
+import threading
 import time
 from tkinter import *
 from tkinter import messagebox
@@ -11,7 +11,6 @@ from db_connection.db_connection_manager import DBConnectionManager
 from db_connection.token_generator import get_token
 from pc_configuration.collect import PCConfiguration
 
-
 DB_CONNECTION_MANAGER = DBConnectionManager()
 
 
@@ -22,6 +21,7 @@ class UIForm(Tk):
         self.title('PCCWidget')
         self.geometry('250x120')
         self.resizable(width=False, height=False)
+        self.protocol('WM_DELETE_WINDOW', self.on_closing)
 
         self.label = Entry(self, state='readonly')
         self.label.pack()
@@ -37,26 +37,32 @@ class UIForm(Tk):
             self.destroy()
 
         self.configuration_object = PCConfiguration(self.token)
-        self.worker = multiprocessing.Process(target=self.update_config_process)
+        self.thread = threading.Thread(target=self.update_config_process)
+        self.thread.daemon = True
 
     def show(self):
         self.mainloop()
+
+    def on_closing(self):
+        if messagebox.askokcancel('Quit', 'Do you want to quit?'):
+            self.destroy()
+            sys.exit()
 
     def connection(self):
         self.button.config(state='disabled')
         try:
             DB_CONNECTION_MANAGER.create(self.configuration_object.config)
+            self.label.configure(state='normal')
+            self.label.delete(0, END)
+            self.label.insert(0, self.token)
+            self.label.configure(state='readonly')
+
+            self.thread.start()
         except (requests.exceptions.RequestException, ValidationError, JSONDecodeError) as exception:
             messagebox.showerror(title='Error',
                                  message='Error while pushing your configuration token on site. '
                                          'Check your internet connection or try again later!')
             self.destroy()
-
-        self.label.configure(state='normal')
-        self.label.delete(0, END)
-        self.label.insert(0, self.token)
-        self.label.configure(state='readonly')
-        self.worker.start()
 
     def update_config_process(self):
         while True:
